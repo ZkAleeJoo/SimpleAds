25608192d1724ca687dde1f645011efb3e81c0be
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-28 19:19:28 -0500

    Update UpdateChecker.java

--- src/main/java/org/zkaleejoo/utils/UpdateChecker.java
+++ src/main/java/org/zkaleejoo/utils/UpdateChecker.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import org.zkaleejoo.SimpleAds;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
@@ -19,7 +19,7 @@ public class UpdateChecker {
 
     public void getVersion(final Consumer<String> consumer) {
         Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); 
+            try (InputStream inputStream = URI.create("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).toURL().openStream(); 
                  Scanner scanner = new Scanner(inputStream)) {
                 if (scanner.hasNext()) {
                     consumer.accept(scanner.next());
